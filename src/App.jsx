560c1e950aed0422c9590059569eb15889c62732
SzymonHarajda <31370918+SzymonHarajda@users.noreply.github.com>
2024-01-02 22:47:04 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+const App=() =>  {
+  return (
+    <div className="categories-container">
+      <div className="categories-container">
+        {
+          /*<img/>*/ 
+          <div className="category-body-container">
+            <h2>Hats</h2>
+            <p>Shop Now</p>
+          </div>
+        }
+      </div>
+      <div className="categories-container">
+        {
+          /*<img/>*/ 
+          <div className="category-body-container">
+            <h2>Jackets</h2>
+            <p>Shop Now</p>
+          </div>
+        }
+      </div>
+      <div className="categories-container">
+        {
+          /*<img/>*/ 
+          <div className="category-body-container">
+            <h2>Snickers</h2>
+            <p>Shop Now</p>
+          </div>
+        }
+      </div>
+      <div className="categories-container">
+        {
+          /*<img/>*/ 
+          <div className="category-body-container">
+            <h2>Women</h2>
+            <p>Shop Now</p>
+          </div>
+        }
+      </div>
+      <div className="categories-container">
+        {
+          /*<img/>*/ 
+          <div className="category-body-container">
+            <h2>Mens</h2>
+            <p>Shop Now</p>
+          </div>
+        }
+      </div>
+    </div>
+  )
+}
+
+export default App;
