eabd1b990a5a36abc274acf34a59dc3b6b777a43
Szymon Harajda <szymonharajda@gmail.com>
2024-01-03 18:48:10 +0100

    updating directory page

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,12 @@
+import Categories from './components/directory/categories.component';
+
 const App=() =>  {
+
   return (
-    <div className="categories-container">
-      <div className="categories-container">
-        {
-          /*<img/>*/ 
-          <div className="category-body-container">
-            <h2>Hats</h2>
-            <p>Shop Now</p>
-          </div>
-        }
-      </div>
-      <div className="categories-container">
-        {
-          /*<img/>*/ 
-          <div className="category-body-container">
-            <h2>Jackets</h2>
-            <p>Shop Now</p>
-          </div>
-        }
-      </div>
-      <div className="categories-container">
-        {
-          /*<img/>*/ 
-          <div className="category-body-container">
-            <h2>Snickers</h2>
-            <p>Shop Now</p>
-          </div>
-        }
-      </div>
-      <div className="categories-container">
-        {
-          /*<img/>*/ 
-          <div className="category-body-container">
-            <h2>Women</h2>
-            <p>Shop Now</p>
-          </div>
-        }
-      </div>
-      <div className="categories-container">
-        {
-          /*<img/>*/ 
-          <div className="category-body-container">
-            <h2>Mens</h2>
-            <p>Shop Now</p>
-          </div>
-        }
-      </div>
-    </div>
-  )
+      <section>
+          <Categories />
+          </section>
+      )  
 }
 
 export default App;
